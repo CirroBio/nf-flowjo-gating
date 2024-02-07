912d3842ff0dc7c63012d7b209e63af676fe4f6f
Sam Minot <sminot@fredhutch.org>
2024-02-06 16:48:28 -0800

    Good first pass

---
+++ main.nf
@@ -0,0 +1,31 @@
+process apply_gates {
+    container "${params.container}"
+    publishDir "${params.output_directory}", mode: 'copy', overwrite: true
+
+    input:
+    path input_fcs
+    path input_wsp
+
+    output:
+    path "*"
+
+    script:
+    template "apply_gates.R"
+}
+
+workflow {
+    Channel
+        .fromPath(
+            "${params.input_fcs}".split(',').toList(),
+            checkIfExists: true
+        )
+        .ifEmpty { error "No files found matching '${params.input_fcs}'" }
+        .set { input_fcs }
+
+    input_wsp = file(params.input_wsp, checkIfExists: true)
+
+    apply_gates(
+        input_fcs,
+        input_wsp
+    )
+}
---
+++ templates/apply_gates.R
@@ -0,0 +1,47 @@
+#!/usr/bin/env Rscript
+
+library(CytoML)
+library(flowWorkspace)
+library(ggcyto)
+library(tidyr)
+
+ws <- open_flowjo_xml("${input_wsp}")
+gh <- flowjo_to_gatingset(
+  ws,
+  includeGates=TRUE,
+  name=1,
+  execute=FALSE
+)
+fcs_files <- list.files(
+  path=".",
+  pattern="*.fcs",
+  full.names=TRUE
+)
+cs <- load_cytoset_from_fcs(files=fcs_files)
+
+print(cs)
+gs <- gh_apply_to_cs(gh, cs)
+
+nodelist <- gs_get_pop_paths(gs, path = "auto")
+for(node in nodelist){
+  if(node != "root"){
+    autoplot(gs, node)
+    ggsave(paste(node, ".pdf", sep=""))
+  }
+}
+
+for(metric in c("percent", "count")){
+  df <- gs_pop_get_stats(gs, type=metric)
+  wide <- pivot_wider(
+    df,
+    id_cols=c("sample"),
+    names_from="pop",
+    values_from=all_of(metric)
+  )
+  write.csv(
+    wide,
+    paste(metric, "csv", sep="."),
+    row.names=FALSE,
+    quote=FALSE
+  )
+}
